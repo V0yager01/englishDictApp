0d530399c8f6873f0938fdb6aac96765661df430
Ilyas Khaliullin <ilyaskhaliullin05@gmail.com>
2025-04-02 13:15:56 +0500

    frontupdates

--- englishDictApp/user/form.py
+++ englishDictApp/user/form.py
@@ -1,9 +1,12 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UsernameField
+from django.contrib.auth.forms import UserCreationForm
 from .models import MyUser, Profile
 
 
 class MyUserCreationForm(UserCreationForm):
+    password1 = forms.CharField(label='Password', widget=forms.TextInput(attrs={"placeholder":"Password"}))
+    password2 = forms.CharField(label='Confirm password',
+                                widget=forms.TextInput(attrs={"placeholder":"Confirm password"}))
     class Meta:
         model = MyUser
         fields = ["username", "email", "password1", "password2"]
@@ -11,11 +14,10 @@ class MyUserCreationForm(UserCreationForm):
             "username": forms.TextInput(attrs={"class": "register-username", "placeholder":"Username"}),
             "email": forms.TextInput(attrs={"class": "register-email", "placeholder":"Email"}),
         }
+     
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields["password1"].widget.attrs["placeholder"] = "Password"
-        self.fields["password2"].widget.attrs["placeholder"] = "Confirm password"
   
 
     def save(self, commit = ...):
